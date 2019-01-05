ff766a189b2f106517e188fd810199abd4698e74
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-05 15:52:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars= ["snickers","hundred grand","kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,element){
-  [`${element}`,...array]
-  return array
+  [`${element}`,...`${array}`]
+  return `${array}`
 }
 
 function destructivelyAddElementToBeginningOfArray(a,b){
