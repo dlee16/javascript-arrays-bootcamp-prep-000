e94e42ec8a27a8defbedff6fce0bd0ccef918995
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-05 16:38:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,8 +11,11 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 }
 
 function addElementToEndOfArray(array, element){
-  
+array=[...array, element]
+return array; 
 }
 
-
-function destructivelyAddElementToEndOfArray(array, element)
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
+}
