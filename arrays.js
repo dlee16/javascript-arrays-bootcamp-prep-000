f2f9f9a5430c9a530330e77cbe7cbcbdb1da4e57
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-05 16:32:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array,element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
- var arr = array;
+ var arr
  arr.unshift(element); 
   
 }
