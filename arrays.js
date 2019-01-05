cb49cb7acc5fe421494fbe22ace32ba481e14cfe
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-05 15:39:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,7 @@ var chocolateBars= ["snickers","hundred grand","kitkat", "skittles"];
 
 function addElementToBeginningOfArray(a,b){
   var chocolateBar =["snickers","hundred grand","kitkat", "skittles"];
-  chocolateBar= ["hershey",...chocolateBar,]
-  
+  ["hershey",...chocolateBar,]
 }
 
 function destructivelyAddElementToBeginningOfArray(a,b)
