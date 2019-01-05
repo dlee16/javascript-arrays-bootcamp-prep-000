d5ab93067bb104664ce07e0a158142ab187adbb5
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-05 15:40:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,6 +3,11 @@ var chocolateBars= ["snickers","hundred grand","kitkat", "skittles"];
 function addElementToBeginningOfArray(a,b){
   var chocolateBar =["snickers","hundred grand","kitkat", "skittles"];
   ["hershey",...chocolateBar,]
+  return chocolateBar
 }
 
-function destructivelyAddElementToBeginningOfArray(a,b)
+function destructivelyAddElementToBeginningOfArray(a,b){
+  var chocolate= ["snickers","hundred grand","kitkat"]
+  chocolate.unshift("hershey")
+  return chocolate;
+}
